74718998aecec37b365d6fcec8a520eae03d84ce
Yuri Moinhos <moinhosyuri@gmail.com>
2024-07-29 17:09:12 -0300

    reupload

---
+++ src/main/java/com/example/demo/domain/service/CadastroCozinhaService.java
@@ -0,0 +1,39 @@
+package com.example.demo.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.exception.EntidadeEmUsoException;
+import com.example.demo.domain.exception.EntidadeNaoEncontradaException;
+import com.example.demo.domain.model.Cozinha;
+import com.example.demo.domain.repository.CozinhaRepository;
+
+@Service
+public class CadastroCozinhaService {
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	@Autowired
+	private CadastroCozinhaService cadastroCozinha;
+	
+	public Cozinha salvar(Cozinha cozinha) {
+		return cozinhaRepository.save(cozinha);
+	}
+	
+	public void excluir(Long cozinhaId) {
+		try {
+			cozinhaRepository.deleteById(cozinhaId);
+			
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não existe um cadastro de cozinha com código %d", cozinhaId));
+			
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(
+				String.format("Cozinha de código %d não pode ser removida, pois está em uso", cozinhaId));
+	}
+  }
+}
